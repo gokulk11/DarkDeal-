d469fd7e10e2d4ffb6f19881dd8e3c2ed493c528
gokulk11 <33909818+gokulk11@users.noreply.github.com>
2024-01-17 00:24:45 +0530

    little changes on orders

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -14,14 +14,14 @@ const OrderItem = () => {
   return (
     <Card my={{base:"1rem", lg:"1.5rem"}}>
       <CardBody display={"flex"} gap={"10px"} justifyContent="space-between">
-        <Box>
+        <Box border={"1px solid"}>
           <Image
-            w={10}
-            h={"50px"}
+            w={16}
+            h={16}
             src="https://rukminim2.flixcart.com/image/850/1000/khavrm80-0/code-in-the-box-game/l/h/h/pc-gta-v-and-indian-army-pc-game-no-cd-dvd-premium-edition-original-imafxc88nepkekhh.jpeg?q=90&crop=false"
           />
         </Box>
-        <Box>
+        <Box border={"1px solid"}>
           <Heading
             as={"h3"}
             size={{ base: "sm" }}
@@ -36,7 +36,7 @@ const OrderItem = () => {
             &#8377;1170.00
           </Text>
         </Box>
-        <Box>
+        <Box border={"1px solid"}>
             <Text>Purchased on Aug 20,2022</Text>
         </Box>
       </CardBody>
